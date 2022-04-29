0db89860696aa3199b58dc45d906b89155a1a450
lichun <lchrennew@126.com>
2022-04-29 21:11:29 +0800

    基本原理

---
+++ root-app/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { getApi } from 'es-fetch-api'
+
+const loadSubApp2 = async () => {
+    const api = getApi(`${location.origin}/sub-app2`)
+    const response = await api('/index.html')
+    const html = await response.text()
+    const div = document.createElement('div')
+    div.innerHTML = html
+    console.log(html);
+    document.body.append(...[ ...div.getElementsByTagName('script') ].map(s => {
+        const script = document.createElement(s.tagName);
+        [ ...s.attributes ].forEach(attr => script.setAttribute(attr.name, attr.value))
+        return script
+    }))
+    // document.body.append(...div.getElementsByTagName('script'))
+
+}
+loadSubApp2()
+</script>
+
+<template>
+    <div id="sub-app1"></div>
+    <div id="sub-app2"></div>
+</template>
+
+<style>
+#root-app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ sub-app1/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    sub-app1
+</template>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
