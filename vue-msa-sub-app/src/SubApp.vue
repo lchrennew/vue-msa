cef6f9a8cde7407e834aa3e7fec8028a987909a1
lichun <lchrennew@126.com>
2022-04-30 09:03:11 +0800

    提取函数到单独的js文件中

--- vue-msa-sub-app/src/SubApp.vue
+++ vue-msa-sub-app/src/SubApp.vue
@@ -1,43 +1,12 @@
 <script setup>
 import { onMounted } from "vue";
-import { getApi } from 'es-fetch-api'
+import { loadApp } from "./sub-app.js";
+
 
 const props = defineProps({
     name: { type: String, required: true },
     base: { type: String, required: true }
 })
-
-const cloneNodes = (root, selector) => [ ...root.querySelectorAll(selector) ].map(el => {
-    const node = document.createElement(el.tagName);
-    [ ...el.attributes ].forEach(attr => node.setAttribute(attr.name, attr.value))
-    return node
-})
-
-const rebaseAssets = (html, base) =>
-    html.replace(
-        /(?<= (?:src|href)=")([^"]+)(?=")/g, match => normalizeUrl(match, base))
-
-const loadAssets = (html, base) => {
-    const div = document.createElement('div')
-    div.innerHTML = rebaseAssets(html, base)
-    document.body.append(...cloneNodes(div, 'script, link[rel="stylesheet"]'))
-}
-
-const normalizeUrl = (url, base = location.origin) => {
-    try {
-        return new URL(url).href
-    } catch (error) {
-        return new URL(url, base).href
-    }
-}
-
-const loadIndex = async ({ base }) => {
-    const api = getApi(normalizeUrl(base))
-    const response = await api('/index.html')
-    return await response.text()
-}
-
-const loadApp = ({ base }) => loadIndex({ base }).then(html => loadAssets(html, base))
 
 const nameDuplicated = document.getElementById(props.name)
 
---
+++ vue-msa-sub-app/src/sub-app.js
@@ -0,0 +1,33 @@
+import { getApi } from 'es-fetch-api'
+
+const cloneNodes = (root, selector) => [ ...root.querySelectorAll(selector) ].map(el => {
+    const node = document.createElement(el.tagName);
+    [ ...el.attributes ].forEach(attr => node.setAttribute(attr.name, attr.value))
+    return node
+})
+
+const rebaseAssets = (html, base) =>
+    html.replace(
+        /(?<= (?:src|href)=")([^"]+)(?=")/g, match => normalizeUrl(match, base))
+
+const loadAssets = (html, base) => {
+    const div = document.createElement('div')
+    div.innerHTML = rebaseAssets(html, base)
+    document.body.append(...cloneNodes(div, 'script, link[rel="stylesheet"]'))
+}
+
+const normalizeUrl = (url, base = location.origin) => {
+    try {
+        return new URL(url).href
+    } catch (error) {
+        return new URL(url, base).href
+    }
+}
+
+const loadIndex = async ({ base }) => {
+    const api = getApi(normalizeUrl(base))
+    const response = await api('/index.html')
+    return await response.text()
+}
+
+export const loadApp = ({ base }) => loadIndex({ base }).then(html => loadAssets(html, base))
